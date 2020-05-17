739f7a75ec019ed99c3c6d5b184f31612fae7433
artisan-tarun <37859575+artisan-tarun@users.noreply.github.com>
2020-05-17 21:52:18 +0530

    initial commit

--- algorithm.php
+++ algorithm.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <?php include('includes/head.php') ?>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <script src="js/algorithm.js"></script>
+
+    <link href="css.algorithm.css" rel="stylesheet">
+
+    <title>App | Home</title>
   </head>
   <body>
-      <?php include('includes/navigation.php') ?>
-    <div class="container-fluid">
-        make algorithms app here code everything inside this page we'll create team page leter
-        after making changes push to github using github desktop app
-    </div>
+      <!-- Brand -->
+      <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <a class="navbar-brand" href="index.php">App</a>
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="algorithm.php">Algorithm</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="container-fluid">
+
+
+          work inside this div.....
+      
+      
+      
+      </div>
   </body>
 </html>
